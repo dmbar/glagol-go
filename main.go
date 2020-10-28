6e944006e9524237daef9fafe6d84915944e708a
dmbar <dmit.barykin@yandex.ru>
2020-10-28 20:45:26 +0300

    Primitive HTTP server with GET Page route

--- main.go
+++ main.go
@@ -2,38 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/bbneizvest/glagol/api/models/page"
+	"net/http"
 )
 
+const apiVersion = "v1"
+
+func handleGETByOID(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "inside Page route\n%q\n%q", r.Method, r.UserAgent())
+}
+
 func main() {
-	// result, err := page.SelectByOID("dbc056a1-3115-4aba-b6ea-8f3269a34cbe", page.CRUD)
-	// if err != nil {
-	// 	log.Fatal("error SelectByOID", err)
-	// }
+	http.HandleFunc(fmt.Sprintf("/api/%s/page", apiVersion), handleGETByOID)
 
-	// fmt.Printf("result: %+v\n", result)
-
-	// jsonData := []byte(`{
-	// 	"header": "BROCKHAMPTON",
-	// 	"body": "NO HALO"
-	// }`)
-	// result, err := page.Insert(jsonData, page.CRUD)
-	// if err != nil {
-	// 	log.Fatal("error Insert", err)
-	// }
-
-	// fmt.Printf("result: %+v\n", result)
-
-	jsonData := []byte(`{
-		"header": "Boards of Canada",
-		"body": "Dayvan Cowboy"
-	}`)
-	result, err := page.Update("7f275b8d-9efa-46ee-8719-48cf6eff04d3", jsonData, page.CRUD)
-	if err != nil {
-		log.Fatal("error Update", err)
-	}
-
-	fmt.Printf("result: %+v\n", result)
+	http.ListenAndServe(":80", nil)
 }
