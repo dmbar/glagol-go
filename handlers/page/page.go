98062975d4f38747eafaab2f4ce552db5ff6644f
dmbar <dmit.barykin@yandex.ru>
2020-11-05 20:19:52 +0300

    call HandleGET in test, mocking rq and rs

--- handlers/page/page.go
+++ handlers/page/page.go
@@ -5,14 +5,8 @@ import (
 	"net/http"
 )
 
+// HandleGET handles all operations for GET method
 func HandleGET(w http.ResponseWriter, rq *http.Request) {
-	fmt.Fprintf(w, "=== Page GET ===\nRequest headers:\n\n")
-	for name, headers := range rq.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%q: %q\n", name, h)
-		}
-	}
+	fmt.Fprintf(w, "good morning slut")
 
-	oid := rq.URL.Query().Get("oid")
-	fmt.Fprintf(w, "\n=== Query ===\nOID: %q", oid)
 }
--- handlers/page/page_test.go
+++ handlers/page/page_test.go
@@ -1,1 +1,23 @@
 package page
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleGET(t *testing.T) {
+	t.Run("error - no OID", func(t *testing.T) {
+		rq, _ := http.NewRequest(http.MethodGet, "/api/v1/page", nil)
+		rs := httptest.NewRecorder()
+
+		HandleGET(rs, rq)
+
+		got := rs.Body.String()
+		want := "good morning slut"
+
+		if got != want {
+			t.Errorf("want %q, got %q", want, got)
+		}
+	})
+}
