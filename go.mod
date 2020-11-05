6d3e472bff9e51bdc1c0e53975c18d8f38f146db
dmbar <dmit.barykin@yandex.ru>
2020-11-05 20:31:11 +0300

    UUID validation for OID

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gofrs/uuid v3.3.0+incompatible // indirect
+	github.com/google/uuid v1.1.2
 	github.com/jackc/pgx/v4 v4.9.0
 	github.com/lib/pq v1.8.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- handlers/page/page.go
+++ handlers/page/page.go
@@ -3,16 +3,21 @@ package page
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
+
+const invalidOID = "OID was not provided or it is not a valid UUID value"
 
 // HandleGET handles all operations for GET method
 func HandleGET(w http.ResponseWriter, rq *http.Request) {
 	oid := rq.URL.Query().Get("oid")
 
-	if oid == "" {
-		fmt.Fprintf(w, "OID was not provided or it is not a valid UUID value")
+	// Validating OID
+	if _, err := uuid.Parse(oid); err != nil {
+		fmt.Fprintf(w, invalidOID)
 		return
 	}
 
-	fmt.Fprintf(w, "good morning slut")
+	fmt.Fprintf(w, "good morning slut") // Call to GetPageByOID
 }
